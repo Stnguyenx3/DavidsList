3bb4e97928539589816185b6af2786a08f3926d3
Imran <iirfan@mail.sfsu.edu>
2016-10-17 21:29:07 -0700

    Used 'id' to link tables together, also removed 'renter' and 'owner' flags, additionally added 'listingID' to tie 'listings' and 'listingID' tables together in install/02-create-table-song.sql. Also made changes to reflect CRUD for all respective tables in model/database.php.

--- _install/02-create-table-song.sql
+++ _install/02-create-table-song.sql
@@ -1,34 +1,37 @@
 CREATE TABLE `student_`.`users` (
+  'id' INT(4) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(100) NOT NULL,
   'password' VARCHAR(64) NOT NULL,
   'studentID' VARCHAR(8),
   `phone` VARCHAR(10),
   `bio` VARCHAR(2000),
-  'renter' TINYINT(1), 
-  'owner' TINYINT(1),
  );
 
 CREATE TABLE 'student_'.'userImages'(
-	'email' VARCHAR(100) NOT NUll,
+	'id' INT(4) NOT NULL,
 	'image' LONGBLOB,
+	'imageThumbnail' SHORTBLOB
 );
 CREATE TABLE 'student_'.'listings' (
-	'email' VARCHAR(100) NOT NULL,
+	'id' INT(4) NOT NULL,
+	'listingID' INT(4) NOT NULL AUTO_INCREMENT,
 	'numberOfBedrooms' INT(4) NOT NULL,
 	'numberOfBathrooms' INT(4) NOT NULL,
 	'internet' TINYINT(1) ,
 	'petPolicy' VARCHAR(100) NOT NULL,
 	'elevatorAccess' text COLLATE utf8_unicode_ci NOT NULL,
-	'furnishing' TINYINT(1),
+	'furnishing' TINYINT(1), #subject to change to a three level int field
 	'airConditioning' TINYINT(1),
 	'address' VARCHAR(100) NOT NULL,
 	'description' VARCHAR(2000) NOT NULL,
 );
 
 CREATE TABLE 'student_'.'listingImages'(
-	'email' VARCHAR(100) NOT NULL,
+	'listingID' INT(4) NOT NULL,
 	'image' LONGBLOB,
+	'imageThumbnail' SHORTBLOB,
 
-  PRIMARY KEY (`email`),
-  UNIQUE KEY `email` (`email`)
+  PRIMARY KEY (`id`),
+  #UNIQUE KEY `email` (`email`)
+  # Used 'id' to link tables together, also removed 'renter' and 'owner' flags, additionally added 'listingID' to tie 'listings' and 'listingID' tables together
 ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
