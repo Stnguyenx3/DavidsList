6163019fd3e44979c26f6a27fbfc648930cd8ce9
Imran <iirfan@mail.sfsu.edu>
2016-11-01 21:59:00 -0700

    Added Google Maps API on search.php within view for testing purposes.

--- application/view/home/search.php
+++ application/view/home/search.php
@@ -4,5 +4,41 @@
 </div>
 
 <div class="container" id="search-result-container">
+	<script>
+		var map;
+		function initMap() {
+			map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 18,
+				center: new google.maps.LatLng(37.7, 122.5),
+				mapTypeId: 'terrain'
+			});	
+
+			//create a script
+			var script = document.createElement('script');
+        	// This example uses a local copy of the GeoJSON stored at
+        	// http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+        	script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+        	document.getElementsByTagName('head')[0].appendChild(script);
+
+		}
+
+		// Loop through the results array and place a marker for each
+      // set of coordinates.
+      window.eqfeed_callback = function(results) {
+        for (var i = 0; i < results.features.length; i++) {
+          var coords = results.features[i].geometry.coordinates;
+          var latLng = new google.maps.LatLng(coords[1],coords[0]);
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+          });
+        }
+      }
+
+	</script>
+
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWOv3nHrDtvumbxi2wfyzUCLJBiV3ax4k&callback=initMap"
+	type="text/javascript"></script>
+
 
 </div>
