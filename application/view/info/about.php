d5104a3294732dc154ccbad6bfc35fd1bcfaabd3
noracato <ncschinkel@gmail.com>
2016-11-01 19:49:23 -0700

    added controller for static pages - check if it works

---
+++ application/view/info/about.php
@@ -0,0 +1,4 @@
+<div class="container" id="image-container">
+    <h2>About us </h2>
+    <p> We're cool </p>
+</div>
---
+++ application/view/info/privacy.php
@@ -0,0 +1,4 @@
+<div class="container" id="image-container">
+    <h2>Privacy and other stuff </h2>
+    <p>  This site is not real. We will sell your information to the highest bidder. </p>
+</div>
