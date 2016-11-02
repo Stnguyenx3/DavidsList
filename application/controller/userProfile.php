29e7c5210c23d015e55c74f93cc9c696582a4a9b
noracato <ncschinkel@gmail.com>
2016-11-01 20:50:15 -0700

    fixed controller for userprofile

--- application/controller/userProfile.php
+++ application/controller/userProfile.php
@@ -14,7 +14,19 @@ class UserProfile extends Controller{
   
     public function index() {
         require APP . 'view/_templates/header.php';
-        require APP . 'view/user/info.php';
+        require APP . 'view/userProfile/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function favorites() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/userProfile/favorites.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function favorites() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/userProfile/listing.php';
         require APP . 'view/_templates/footer.php';
     }
 
