2abe3c888462d89a25127d9ca1c988b9b1c695dd
noracato <ncschinkel@gmail.com>
2016-11-01 20:59:22 -0700

    changed user to userProfile

--- application/view/userProfile/index.php
+++ application/view/userProfile/index.php
@@ -1,7 +1,7 @@
 <!-- mock -->
 <div class="container" id="blah">
-	<a href="<?php echo URL."user/favorites"; ?>">favorites</a>
-    <a href="<?php echo URL."user/listings" ; ?>">my listings</a>
+	<a href="<?php echo URL."userProfile/favorites"; ?>">favorites</a>
+    <a href="<?php echo URL."userProfile/listings" ; ?>">my listings</a>
     <a href="<?php echo URL."home/rentout"; ?>">rent out</a>
 </div>
 <div class="container" id="image-container">
