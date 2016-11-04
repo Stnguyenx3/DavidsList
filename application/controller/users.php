9a3e8482fd561b3ef54dffc803fb23604d3ca575
Paul Derugin <pderugin@mail.sfsu.edu>
2016-11-04 16:57:41 -0700

    Added function deleteUser() to the Users Controller (havent' checked if it works yet). Added two pages: error_page.php and user_delete_success.php.

--- application/controller/users.php
+++ application/controller/users.php
@@ -14,21 +14,58 @@
 class Users extends Controller {
 
     /**
-     * Returns a user's profile page.
+     * Loads a user's profile page.
      * 
      * @param int $userID The integer ID of the user
-     * 
-     * @return The user's HTML profile page
      */
     public function getUserProfilePage($userID) {
         $userRepo = RepositoryFactory::createRepository("user");
-        $arrayOfUserObjects = $userRepo->find($userID, "userid"); // an array of User objects
-        // Now how to return an HTML page from a User object?
-        // Do somethign along the lines of:
-        //What these three lines will do is, it will send back the header, body, and footer of a webpage. # which webpage?
-        require APP . "view/_templates/header.php";
-        require APP . "view/users/user_body.php"; // create this
-        require APP . "view/_templates/footer.php";
+        $arrayOfUserObjects = $userRepo->find($userID, "userid"); // an array of 
+                                                                  // User objects
+        
+        if ($arrayOfUserObjects == null){
+            // TODO add more detail to the error page
+            require APP . "view/problem/error_page.php";
+        }
+        
+        else{
+            // The folowing three lines send back the header, body, and footer
+            // of the user_body.php webpage
+            require APP . "view/_templates/header.php";
+            require APP . "view/users/user_body.php"; // a file that for now
+                                                      // just displays the
+                                                      // user's email address.
+            require APP . "view/_templates/footer.php";            
+        } 
+    } // end function getUserProfilePage
+    
+    
+    /**
+     * Deletes a user from the database.
+     * 
+     * @param int $userID
+     * 
+     * @todo test this function.
+     */
+    public function deleteUser($userID) {
+        $userRepo = RepositoryFactory::createRepository("user");
+        $arrayOfUserObjects = $userRepo->find($userID, "userid"); // an array of 
+                                                                  // User objects
+        if ($arrayOfUserObjects == null){
+            // TODO add more detail to the error page            
+            require APP . "view/problem/error_page.php";
+        }
+        
+        else{
+            $userRepo->remove($arrayOfUserObjects[0]);
+            
+            require APP . "view/_templates/header.php";
+            require APP . "view/users/user_delete_success.php"; // this page
+                                                                // confirms the 
+                                                                // deletion of 
+                                                                // the user
+            require APP . "view/_templates/footer.php";               
+        }
     }
-
+    
 }
---
+++ application/view/problem/error_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>"Error"</title>
+    </head>
+    <body>
+        <!-- 
+        Put some shit here to tell the user what the error is. For example,
+        the page does not exist, or the user was not found, etc.
+        -->
+    </body>
+</html>
