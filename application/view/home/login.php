8c239766332d555aa92c185287b9d1a844d7ec47
Steven Nguyen <stevennguyenx3@gmail.com>
2016-11-06 14:36:24 -0800

    Improved design of login page.

--- application/view/home/login.php
+++ application/view/home/login.php
@@ -1,17 +1,21 @@
 <body>
 	<div class="container">
 		
-        <div class="card" style="text-align:center">
-            
-            <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-            
-            <form class="form-signin">
+        <div class="card">
+
+            <form class="form-signin center-block">
                 
                 <input type="email" id="inputEmail" class="form-control center-block" placeholder="Email address" required autofocus>
 				
                 <input type="password" id="inputPassword" class="form-control center-block" placeholder="Password" required>
 				
-                <button class="btn btn-lg btn-primary btn-signin" type="submit">Sign in</button>
+                <button class="btn btn-lg btn-success btn-signin center-block" type="submit">Sign in</button>
+				
+				<div class="login-link">
+					<a href="<?php echo URL. "home/register" ; ?>">Sign up!</a>
+					
+					<a href="#">Forgot Password?</a>
+				</div>
 				
             </form>
 
