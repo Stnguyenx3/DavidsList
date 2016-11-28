7872397d1238f3b275708d1d9e445051814577e9
Steven Nguyen <stevennguyenx3@gmail.com>
2016-11-27 22:45:24 -0800

    Improved home page.

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,6 +1,6 @@
 <div class="container main">
 	<div class="row">
-		<div class="col-sm-12">
+		<div class="col-md-12">
 			<h1 class="centered-header">Welcome To DavidsList! Rent A Room and Stay A While</h1>
 			<p style="padding-left: 100px; padding-right: 100px; font-size: 16px;">
 				DavidsList is an apartment rental website created by SFSU students for SFSU students. We believe that
@@ -11,78 +11,160 @@
 			</p>
 		</div>
 	</div>
-	
-	<div class="row">
-		<div class="col-sm-1"></div>
-		<div class="col-sm-10">
-			<h1 class="centered-header">Featured Listings</h1>
-			<div class="row featured-listing linear-gradient-bg">
-				<div class="col-sm-12">
-					<div class="row">
-						<div class="col-sm-3 featured-listing-img">
-							<img src= "<?php echo $newListingImages[0] ?>" alt="placeholder img." width="175" height="175">
-						</div>
-						<div class="col-sm-9">
-							<h3 class="featured-listing-title"><?php echo $newListings["addresses"][0]->getStreetName() ?></h3>
-							<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][0]->getPrice() ?></p>
-							<div style="clear: both">
-									<ul class="listing-basic-info">
-										<li>Bed: <?php echo $newListings["listing_details"][0]->getNumberOfBedrooms() ?></li>
-										<li>Bath: <?php echo $newListings["listing_details"][0]->getNumberOfBathrooms() ?></li>
-										<li>Distance To SFSU: #TODO</li>
-									</ul>
-									<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][0]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row featured-listing linear-gradient-bg">
-				<div class="col-sm-12">
-					<div class="row">
-						<div class="col-sm-3 featured-listing-img">
-							<img src="<?php echo $newListingImages[1] ?>" alt="placeholder img."  width="175" height="175">
-						</div>
-						<div class="col-sm-9">
-							<h3 class="featured-listing-title"><?php echo $newListings["addresses"][1]->getStreetName() ?></h3>
-							<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][1]->getPrice() ?></p>
-							<div style="clear: both">
-									<ul class="listing-basic-info">
-										<li>Bed: <?php echo $newListings["listing_details"][1]->getNumberOfBedrooms() ?></li>
-										<li>Bath: <?php echo $newListings["listing_details"][1]->getNumberOfBathrooms() ?></li>
-										<li>Distance To SFSU: #TODO</li>
-									</ul>
-									<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][1]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row featured-listing linear-gradient-bg">
-				<div class="col-sm-12">
-					<div class="row">
-						<div class="col-sm-3 featured-listing-img">
-							<img src= "<?php echo $newListingImages[2] ?>" alt="placeholder img." width="175" height="175">
-						</div>
-						<div class="col-sm-9">
-							<h3 class="featured-listing-title"><?php echo $newListings["addresses"][2]->getStreetName() ?></h3>
-							<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][2]->getPrice() ?></p>
-							<div style="clear: both">
-									<ul class="listing-basic-info">
-										<li>Bed: <?php echo $newListings["listing_details"][2]->getNumberOfBedrooms() ?></li>
-										<li>Bath: <?php echo $newListings["listing_details"][2]->getNumberOfBathrooms() ?></li>
-										<li>Distance To SFSU: #TODO</li>
-									</ul>
-									<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][2]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
 
-									<script src="https://maps.googleapis.com/maps/api/distancematrix/json?origins=San+Francisco&destinations=Seattle&mode=bicycling&language=fr-FR&key=AIzaSyBqaRtjuh9yOQakPVXhLEgEaBhmAMv9GmA"></script>
-									
-							</div>
-						</div>
-					</div>
+	<div class="row">
+
+		<div class="col-md-4">
+
+			<div class="featured-listing-container custom-border linear-gradient-bg" style="overflow:hidden">
+			
+				<img class="featured-listing-img" src="<?php echo $newListingImages[0] ?>" alt="Image missing." style="width: 320 ; height: 160;">
+
+				<h3 class="featured-listing-title"><?php echo $newListings["addresses"][0]->getStreetName() ?></h3>
+
+				<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][0]->getPrice() ?></p>
+
+				<div>
+					<ul class="featured-listing-info">
+						<li>Bed: <?php echo $newListings["listing_details"][0]->getNumberOfBedrooms() ?></li>
+						<li>Bath: <?php echo $newListings["listing_details"][0]->getNumberOfBathrooms() ?></li>
+						<li>Distance To SFSU: #TODO</li>
+					</ul>
 				</div>
+
+				<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][0]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
+
 			</div>
+
 		</div>
-		<div class="col-sm-1"></div>
+
+		<div class="col-md-4">
+
+			<div class="featured-listing-container custom-border linear-gradient-bg" style="overflow:hidden">
+			
+				<img class="featured-listing-img" src="<?php echo $newListingImages[1] ?>" alt="Image missing." style="width: 320 ; height: 160;">
+
+				<h3 class="featured-listing-title"><?php echo $newListings["addresses"][1]->getStreetName() ?></h3>
+
+				<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][1]->getPrice() ?></p>
+
+				<div>
+					<ul class="featured-listing-info">
+						<li>Bed: <?php echo $newListings["listing_details"][1]->getNumberOfBedrooms() ?></li>
+						<li>Bath: <?php echo $newListings["listing_details"][1]->getNumberOfBathrooms() ?></li>
+						<li>Distance To SFSU: #TODO</li>
+					</ul>
+				</div>
+
+				<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][1]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
+
+			</div>
+
+		</div>
+
+		<div class="col-md-4">
+
+			<div class="featured-listing-container custom-border linear-gradient-bg" style="overflow:hidden">
+			
+				<img class="featured-listing-img" src="<?php echo $newListingImages[2] ?>" alt="Image mising." style="width: 320 ; height: 160;">
+
+				<h3 class="featured-listing-title"><?php echo $newListings["addresses"][2]->getStreetName() ?></h3>
+
+				<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][2]->getPrice() ?></p>
+
+				<div>
+					<ul class="featured-listing-info">
+						<li>Bed: <?php echo $newListings["listing_details"][2]->getNumberOfBedrooms() ?></li>
+						<li>Bath: <?php echo $newListings["listing_details"][2]->getNumberOfBathrooms() ?></li>
+						<li>Distance To SFSU: #TODO</li>
+					</ul>
+				</div>
+
+				<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][2]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
+
+			</div>
+
+		</div>
+
 	</div>
+
+	<div class="row">
+
+		<div class="col-md-4">
+
+			<div class="featured-listing-container custom-border linear-gradient-bg" style="overflow:hidden">
+			
+				<img class="featured-listing-img" src="<?php echo $newListingImages[3] ?>">
+
+				<h3 class="featured-listing-title"><?php echo $newListings["addresses"][3]->getStreetName() ?></h3>
+
+				<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][3]->getPrice() ?></p>
+
+				<div>
+					<ul class="featured-listing-info">
+						<li>Bed: <?php echo $newListings["listing_details"][3]->getNumberOfBedrooms() ?></li>
+						<li>Bath: <?php echo $newListings["listing_details"][3]->getNumberOfBathrooms() ?></li>
+						<li>Distance To SFSU: #TODO</li>
+					</ul>
+				</div>
+
+				<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][3]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
+
+
+			</div>
+
+		</div>
+
+		<div class="col-md-4">
+
+			<div class="featured-listing-container custom-border linear-gradient-bg" style="overflow:hidden">
+			
+				<img class="featured-listing-img" src="<?php echo $newListingImages[4] ?>">
+
+				<h3 class="featured-listing-title"><?php echo $newListings["addresses"][4]->getStreetName() ?></h3>
+
+				<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][4]->getPrice() ?></p>
+
+				<div>
+					<ul class="featured-listing-info">
+						<li>Bed: <?php echo $newListings["listing_details"][4]->getNumberOfBedrooms() ?></li>
+						<li>Bath: <?php echo $newListings["listing_details"][4]->getNumberOfBathrooms() ?></li>
+						<li>Distance To SFSU: #TODO</li>
+					</ul>
+				</div>
+
+				<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][4]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
+
+
+			</div>
+
+		</div>
+
+		<div class="col-md-4">
+
+			<div class="featured-listing-container custom-border linear-gradient-bg" style="overflow:hidden">
+			
+				<img class="featured-listing-img" src="<?php echo $newListingImages[5] ?>">
+
+				<h3 class="featured-listing-title"><?php echo $newListings["addresses"][5]->getStreetName() ?></h3>
+
+				<p class="featured-listing-price">Price: $<?php echo $newListings["listings"][5]->getPrice() ?></p>
+
+				<div>
+					<ul class="featured-listing-info">
+						<li>Bed: <?php echo $newListings["listing_details"][5]->getNumberOfBedrooms() ?></li>
+						<li>Bath: <?php echo $newListings["listing_details"][5]->getNumberOfBathrooms() ?></li>
+						<li>Distance To SFSU: #TODO</li>
+					</ul>
+				</div>
+
+				<a href="<?php echo URL . "listings/getlisting/" . $newListings["listings"][5]->getListingId() ?>" class="btn btn-primary featured-listing-btn">Rent</a>
+
+
+			</div>
+
+		</div>
+
+	</div>
+
 </div>
