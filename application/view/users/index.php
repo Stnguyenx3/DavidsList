9c65ddc105f2097222ae1867e85aebb05c8b0db2
noracato <ncschinkel@gmail.com>
2016-12-09 13:24:33 -0800

    restyled profile page to match rest of website

--- application/view/users/index.php
+++ application/view/users/index.php
@@ -1,9 +1,9 @@
-<div class="container" style="margin-top: 25px">
+<div class="container main">
 	<div class="row">
 
 	<div class="col-md-1"></div>
 
-		<div class="col-md-10 user-overview custom-border linear-gradient-bg">
+		<div class="col-md-10 user-overview">
 
 			<h2 class="centered-header">Profile</h2>
 
