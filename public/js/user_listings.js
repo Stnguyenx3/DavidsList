a5d49a845502808cc3fdfc0e114f2c5bdd6f1b22
noracato <ncschinkel@gmail.com>
2016-12-08 22:03:26 -0800

    added no result message for user listings

--- public/js/user_listings.js
+++ public/js/user_listings.js
@@ -35,49 +35,58 @@ function formatUserListings(event) {
 
 		var numOfListings = event.length;
 
-		for (var i = 0; i < numOfListings; i++){
+		if(numOfListings == 0){
+			//Display no result message to user.
+			var row = $("<div></div>").addClass("row search-result-listing-null").appendTo($("#listings"));
+			$("<div></div>").addClass("col-sm-3").appendTo($(row));
+			var col = $("<div></div>").addClass("col-sm-6").appendTo($(row));
+			var message = $("<p></p>").addClass("search-result-listing-null no-center").appendTo($(col));
+			$(message).text("You currently have no listing posted. If you want to create a listing on DavidsList, please click the 'Create New' button.");
 
-			var row0 = $("<div></div>").addClass("row user-listing linear-gradient-bg custom-border").appendTo($("#listings"));
+		} else {
+			for (var i = 0; i < numOfListings; i++){
 
-			//Add unique ID for each row (remove if not needed!)
-			$(row0).attr("id", "user-listing-" + event[i].listing.listingId);
+				var row0 = $("<div></div>").addClass("row user-listing linear-gradient-bg custom-border").appendTo($("#listings"));
 
-			var col0 = $("<div></div>").addClass("col-sm-12").appendTo($(row0));
-			var row1 = $("<div></div>").addClass("row").appendTo($(col0));
-			var col1 = $("<div></div>").addClass("col-sm-3 user-listing-img").appendTo($(row1));
-			var img = $("<img></img>").appendTo($(col1));
-			var col2 = $("<div></div>").addClass("col-sm-9").appendTo($(row1));
-			var h3 = $("<h3></h3>").addClass("user-listing-title").appendTo($(col2));
-			var p0 = $("<p></p>").addClass("user-listing-price").appendTo($(col2)); //Represents the price of listing
-			var div = $("<div></div>").css("clear", "both").appendTo($(col2));
-			var p1 = $("<p></p>").appendTo($(div));
-			var a0 = $("<a></a>").addClass("btn btn-primary user-listings-edit")
-						.click({listingId: event[i].listing.listingId}, onClickEditListing)
-						.appendTo($(div));
-			var a1 = $("<a></a>").addClass("btn btn-primary user-listings-remove")
-						.click({listingId: event[i].listing.listingId}, onClickDeleteListing)
-						.appendTo($(div));
-			var a2 = $("<a></a>").addClass("btn btn-primary user-messages")
-						.click({listingId: event[i].listing.listingId}, onClickMessages)
-						.appendTo($(div));
+				//Add unique ID for each row (remove if not needed!)
+				$(row0).attr("id", "user-listing-" + event[i].listing.listingId);
 
-			//Store listing inforation into variables.
-			var listingImg = "data:image/png;base64,"+event[i].listing_images;
-			var listingTitle = event[i].address.streetName + " " + event[i].address.city + " " + event[i].address.state + ", " + event[i].address.zipcode;
-			var listingPrice = event[i].listing.price;
-			var listingDescription = event[i].listing_detail.description;
+				var col0 = $("<div></div>").addClass("col-sm-12").appendTo($(row0));
+				var row1 = $("<div></div>").addClass("row").appendTo($(col0));
+				var col1 = $("<div></div>").addClass("col-sm-3 user-listing-img").appendTo($(row1));
+				var img = $("<img></img>").appendTo($(col1));
+				var col2 = $("<div></div>").addClass("col-sm-9").appendTo($(row1));
+				var h3 = $("<h3></h3>").addClass("user-listing-title").appendTo($(col2));
+				var p0 = $("<p></p>").addClass("user-listing-price").appendTo($(col2)); //Represents the price of listing
+				var div = $("<div></div>").css("clear", "both").appendTo($(col2));
+				var p1 = $("<p></p>").appendTo($(div));
+				var a0 = $("<a></a>").addClass("btn btn-primary user-listings-edit")
+							.click({listingId: event[i].listing.listingId}, onClickEditListing)
+							.appendTo($(div));
+				var a1 = $("<a></a>").addClass("btn btn-primary user-listings-remove")
+							.click({listingId: event[i].listing.listingId}, onClickDeleteListing)
+							.appendTo($(div));
+				var a2 = $("<a></a>").addClass("btn btn-primary user-messages")
+							.click({listingId: event[i].listing.listingId}, onClickMessages)
+							.appendTo($(div));
 
-			//Insert listing information into HTML elements.
-			$(h3).text(listingTitle);
-			$(img).attr("src", listingImg).attr("width", "175").attr("height", "175");
-			$(p0).text("Price: $" + listingPrice);
-			$(p1).text("Description:"+ listingDescription);
-			$(a0).text("Edit");
-			$(a1).text("Remove");
-			$(a2).text("Messages");
+				//Store listing inforation into variables.
+				var listingImg = "data:image/png;base64,"+event[i].listing_images;
+				var listingTitle = event[i].address.streetName + " " + event[i].address.city + " " + event[i].address.state + ", " + event[i].address.zipcode;
+				var listingPrice = event[i].listing.price;
+				var listingDescription = event[i].listing_detail.description;
 
+				//Insert listing information into HTML elements.
+				$(h3).text(listingTitle);
+				$(img).attr("src", listingImg).attr("width", "175").attr("height", "175");
+				$(p0).text("Price: $" + listingPrice);
+				$(p1).text("Description:"+ listingDescription);
+				$(a0).text("Edit");
+				$(a1).text("Remove");
+				$(a2).text("Messages");
+
+			}
 		}
-		
 	});
 
 }
